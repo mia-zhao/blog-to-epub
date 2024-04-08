67419aec3d62928ee55ef8b80c6e3b537bbf890e
Mia Zhao <miayunyizhao@gmail.com>
2024-04-08 14:07:48 -0700

    use daisyui

--- src/options/main.tsx
+++ src/options/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Options from "./Options";
+import "./Options.css";
 
 ReactDOM.createRoot(document.getElementById("options")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,11 @@ import manifest from "./manifest.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest: manifest as ManifestV3Export })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
 });
